8d80de2f9146df329c117288d1a7baaef5ca424a
Mahima Rai <mahimabayingrai22@gmail.com>
2026-04-15 16:26:31 +0000

    update

--- src/generator.py
+++ src/generator.py
@@ -36,7 +36,14 @@ class MultimodalGenerator:
             
         # )
 
-        self.reader = easyocr.Reader(['en'], gpu=True, model_storage_directory="easyocr_models")
+        # self.reader = easyocr.Reader(['en'], gpu=True, model_storage_directory="easyocr_models")
+        self.reader = easyocr.Reader(
+            ['en'],
+            gpu=torch.cuda.is_available(),
+            model_storage_directory="easyocr_models"
+        )
+
+        self.pdf_cache = {}
     
     def _extract_text(self, image: Image.Image) -> str:
         image=image.convert("RGB")
